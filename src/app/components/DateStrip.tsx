6b668e1274a3db7918fc48669f9a4e830cdc515a
Gini <gini@MacBook-Pro-2.local>
2026-01-11 00:23:45 +0400

    123

--- src/app/components/DateStrip.tsx
+++ src/app/components/DateStrip.tsx
@@ -152,10 +152,10 @@ export function DateStrip({
                   isSelected ? "bg-secondary" : "hover:bg-muted/10"
                 }`}
               >
-                <div className="text-[48px] #000 font-bold">
+                <div className="text-[48px] color-#000 font-bold">
                   {String(date.getDate()).padStart(2, "0")}
                 </div>
-                <div className="text-[13px] #000">
+                <div className="text-[13px] color-#000">
                   {getMonthName(date.getMonth())} {date.getFullYear()}
                 </div>
               </button>
